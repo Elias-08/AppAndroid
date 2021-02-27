457aa84ab171d045d9eab0364aad6408eaddc431
Elias-08 <58613321+Elias-08@users.noreply.github.com>
2021-02-26 21:11:28 -0400

    Initial commit

---
+++ AppAndroid/AppAndroid/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppAndroid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Al inicializar este componente que esta en el lenguaje Xaml le cambio el color desde aca
+            lblPrincipal.TextColor = Color.Blue;
+            var x = lblPrincipal.Width;
+        }
+
+        private void OnClick(object sender, EventArgs e)
+        {
+            //En esta condicional con Getvalueobtengo el valor o color de lbl y con set value le mando un nuevo color
+            if ((Color) lblPrincipal.GetValue(Label.TextColorProperty)== Color.Blue)
+            {
+                lblPrincipal.SetValue(Label.TextColorProperty, Color.Red);
+            }
+            else if((Color)lblPrincipal.GetValue(Label.TextColorProperty) == Color.Red)
+            {
+                lblPrincipal.SetValue(Label.TextColorProperty, Color.Aqua);
+            } 
+            else if ((Color)lblPrincipal.GetValue(Label.TextColorProperty) == Color.Aqua)
+            {
+                lblPrincipal.SetValue(Label.TextColorProperty, Color.BlueViolet);
+            }
+            else if ((Color)lblPrincipal.GetValue(Label.TextColorProperty) == Color.BlueViolet)
+            {
+                lblPrincipal.SetValue(Label.TextColorProperty, Color.Gold);
+
+            }
+            else if ((Color)lblPrincipal.GetValue(Label.TextColorProperty) == Color.Gold)
+            {
+                lblPrincipal.SetValue(Label.TextColorProperty, Color.Blue);
+            }
+
+        }
+
+        //Este metodo me sirve para navegar a mi otra pagina
+        async void OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+
+        //Este es una manera de invocar un Metodo, Tambien desde el constructor 
+        //se  puede llamar el nombre de Objeto EJ: Boton.Clicked += y el nombre del Evento, asi no llamarlo desde XAML
+        private void Hora(object sender, EventArgs e)
+        {
+            lblSencundario.TextColor = Color.Black;
+            lblSencundario.Text = "La Hora y fecha es : " + DateTime.Now.ToString();
+        }
+    }
+}
---
+++ AppAndroid/AppAndroid/Page1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppAndroid
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void OnClick(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            //Aunque se puede hacer de esta manera en propiedades Adjuntas, no es buena practica
+            //Algunas como el grid ya tienen propiedad GetValue y SetValue
+
+            //var column = (int)button.GetValue(Grid.ColumnProperty);
+
+            //if (column==0)
+            //{
+            //    button.SetValue(Grid.ColumnProperty, 1);
+            //}
+            //else
+            //{
+            //    button.SetValue(Grid.ColumnProperty, 0);
+            //}
+
+            if (Grid.GetColumn(button)==0)
+            {
+                Grid.SetColumn(button, 1);
+            }
+            else
+            {
+                Grid.SetColumn(button, 0);
+
+            }
+
+            //es mejor utilizar los getters/setters de las propiedades adjuntas.
+        }
+    }
+}
