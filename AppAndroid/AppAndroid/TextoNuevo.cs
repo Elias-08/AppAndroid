3125b24467225d99ec8d06dcffcacdaa67422124
Elias-08 <58613321+Elias-08@users.noreply.github.com>
2021-03-11 18:57:21 -0400

    Cambios

---
+++ AppAndroid/AppAndroid/TextoNuevo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppAndroid
+{
+    // Esta clase me sirve como una Extension de Marcado, Le agrege Internal
+    //Asi puedo Acceder a ella de manera Facil desde lenguaje XAML
+    internal class TextoNuevo
+    {
+        public static string Message = "Muestro este Texto desde una Extension Static";
+    }
+}
